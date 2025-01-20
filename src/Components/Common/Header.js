83ed5a69b5fe4104d0d60d88ebe1313681f029dc
Rohite <rohitpk5101994@gmail.com>
2025-01-20 23:27:42 +0530

    header jsx added

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Header() {
     return(
          <div>
-            
+             <h2>Header</h2>
+             <p>i have added the header section</p>
          </div>
 
     )
